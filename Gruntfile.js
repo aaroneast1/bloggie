dab42c18c7f1a4ef4a98d321f6ddc7ef999bf6aa
Aaron E <aaron.east@expressivesoftware.co.uk>
2013-09-24 15:31:31 +0100

    Added mocha tasks to grunt to run tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,17 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    simplemocha: {
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        grep: '*-test',
+        ui: 'bdd',
+        reporter: 'tap'
+      },
+      all: { src: ['test/**/*.js'] }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -16,8 +27,9 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-simple-mocha');
 
-  // Default task(s).
+  // Default task(s). (optional defaults)
   grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('default', 'simplemocha');
 };
